cd5a1ffe7b096a0371d6de3a8f01856a91afeeb9
BENJAMIN <ambluse@163.com>
2017-10-18 01:37:36 -0500

    Update mom-data.sql

--- mom-client/mom-client/sql/mom-data.sql
+++ mom-client/mom-client/sql/mom-data.sql
@@ -5,8 +5,8 @@ CREATE TABLE `mom_data` (
 	`msg_key` CHAR(36) NOT NULL COMMENT '唯一标识',
 	`data` MEDIUMTEXT NOT NULL COMMENT '最大16MB，UTF-8',
 	`status` TINYINT(4) NOT NULL DEFAULT '0',
-	`infoMsg` VARCHAR(256) NOT NULL DEFAULT '',
-	`consumeTime` BIGINT(20) NOT NULL DEFAULT '0' COMMENT '上次消耗时间，精确到毫妙',
+	`info_msg` VARCHAR(256) NOT NULL DEFAULT '',
+	`consume_time` BIGINT(20) NOT NULL DEFAULT '0' COMMENT '上次消耗时间，精确到毫妙',
 	`retryCount` INT(11) NOT NULL DEFAULT '0' COMMENT '重试次数',
 	`exchange` VARCHAR(50) NOT NULL DEFAULT '',
 	`routerKey` VARCHAR(50) NOT NULL DEFAULT '',
@@ -14,7 +14,6 @@ CREATE TABLE `mom_data` (
 	INDEX `correlation_id` (`correlation`),
 	INDEX `status` (`status`)
 )
-COMMENT='pikaq数据存储'
 COLLATE='utf8_general_ci'
 ENGINE=InnoDB
 ;
