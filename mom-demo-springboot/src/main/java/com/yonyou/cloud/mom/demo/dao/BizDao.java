b6a8946beac07f19378061e206f1e95541269459
10433918 <32384033+10433918@users.noreply.github.com>
2025-05-20 10:37:55 +0800

    Delete mom-demo-springboot directory

--- mom-demo-springboot/src/main/java/com/yonyou/cloud/mom/demo/dao/BizDao.java
+++
@@ -1,9 +0,0 @@
-package com.yonyou.cloud.mom.demo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.yonyou.cloud.mom.demo.msg.entity.BizEntity;
-
-public interface BizDao extends JpaRepository<BizEntity, String>{
-
-}
